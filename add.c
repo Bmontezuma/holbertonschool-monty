d8d805bc1cf26e3224e58a43976b12e3edffa2f3
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-10 09:55:14 -0800

    Updated Files

--- add.c
+++ add.c
@@ -9,21 +9,21 @@
  */
 void push(stack_t **stack, int value)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+    stack_t *new_node = malloc(sizeof(stack_t));
 
-	if (new_node == NULL)
-	{
-		handle_error(MALLOC_ERROR, 0);
-		return;
-	}
+    if (new_node == NULL)
+    {
+        handle_error(MALLOC_ERROR, 0);
+        return;
+    }
 
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
 
-	*stack = new_node;
+    *stack = new_node;
 }
 
