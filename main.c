6e94159f046944610b042b851ae0c42248e76485
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-10 07:16:29 -0800

    Updated Files

--- main.c
+++ main.c
@@ -12,36 +12,35 @@ void free_stack(stack_t *stack);
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    unsigned int line_number = 0;
-    stack_t *stack = NULL;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	exit(EXIT_FAILURE);
+	}
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        line_number++;
-        execute_opcode(&stack, line, line_number);
-    }
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+	line_number++;
+	execute_opcode(&stack, line, line_number);
+	}
 
-    free(line);
-    fclose(file);
-    free_stack(stack);
+	free(line);
+	fclose(file);
+	free_stack(stack);
 
-    return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
-
--- monty.h
+++ monty.h
@@ -16,9 +16,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,13 +31,15 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* Prototypes */
 void push(stack_t **stack, int value);
 void pall(stack_t **stack);
+void execute_opcode(stack_t **stack, char *opcode, unsigned int line_number);
+void free_stack(stack_t *stack);
 
 #endif /* MONTY_H */
 
