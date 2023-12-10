190993084be4c6bc26aef61de948fc6254252536
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-09 16:44:14 -0800

    Push, Pall

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+        /* Check for number of arguments */
+        if (argc != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                return (EXIT_FAILURE);
+        }
+
+        /* Open file */
+        FILE *file = fopen(argv[1], "r");
+        if (!file)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+                return (EXIT_FAILURE);
+        }
+        /* Close file */
+        fclose(file);
+
+        return (EXIT_SUCCESS);
+}
+
