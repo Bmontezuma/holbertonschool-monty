dde068594cb02d03ee434dbad3cae7be6fd9f205
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-16 13:56:34 -0800

    Updated Files

--- free_memory.c
+++ free_memory.c
@@ -33,3 +33,23 @@ void free_args_tokens_stack(int argc, char **argv, char **tokens, stack_t **stac
 	/* Free the stack */
 	free_stack(stack);
 }
+/**
+ * free_stack - Free the memory used by a stack.
+ * @stack: A pointer to the stack.
+ */
+void free_stack(stack_t **stack)
+{
+    stack_t *current, *temp;
+
+    if (*stack != NULL)
+    {
+        current = *stack;
+        while (current != NULL)
+        {
+            temp = current;
+            current = current->next;
+            free(temp);
+        }
+        *stack = NULL; /* Set the stack pointer to NULL after freeing all nodes */
+    }
+}
