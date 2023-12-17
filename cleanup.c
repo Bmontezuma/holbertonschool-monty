2179302503d0648cd39dac2923cf38a64474bcc3
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-17 08:34:39 -0800

    Updated

--- cleanup.c
+++ cleanup.c
@@ -1,18 +1,16 @@
 #include "monty.h"
-#include <stdlib.h>
 
 /**
  * cleanup - Frees the memory occupied by the stack
  * @stack: Pointer to the top of the stack
  */
-void cleanup(Node *stack)
+void cleanup(stack_t *stack)
 {
-    Node *temp;
-
-    while (stack != NULL)
+    while (stack)
     {
-        temp = stack;
+        stack_t *temp = stack;
         stack = stack->next;
         free(temp);
     }
 }
+
