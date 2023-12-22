8018407f57bb3cc0a4d5d3f61c55921d5f06ca8f
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-21 19:25:34 -0800

    Updated Files

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * main - Entry point of the Monty interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    stack_t *stack = NULL;
+    unsigned int line_number = 0;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+
+    }
+
+    free(line);
+    fclose(file);
+    freeStack(&stack);
+
+    return 0;
+}
+
